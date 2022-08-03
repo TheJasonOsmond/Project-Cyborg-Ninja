a71f07d81339755cc469966b65739315c7dfe78a
Jason Osmond <jasonosmond2001@gmail.com>
2022-08-02 18:26:12 -0600

    Enemy Movement Animations
    Enemy movement is animated

--- Project Cyborg Ninja/Assets/Scripts/Enemy_AI.cs
+++ Project Cyborg Ninja/Assets/Scripts/Enemy_AI.cs
@@ -7,6 +7,9 @@ public class Enemy_AI : MonoBehaviour
 {
 
     public Transform target;
+    public Transform enemyGFX;
+
+    [SerializeField] protected Animator animator;
 
     public float moveSpeed = 200f; 
     [SerializeField] float nextWaypointDistance = 0.1f;
@@ -18,15 +21,18 @@ public class Enemy_AI : MonoBehaviour
     Seeker seeker;
     Rigidbody2D rb;
 
+    bool facingRight;
+
+
     // Start is called before the first frame update
     void Start()
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
 
-        InvokeRepeating("UpdatePath", 0f, 0.5f);
+        InvokeRepeating("UpdatePath", 0f, 0.5f); //Update path every 0.5 seconds
         
-
     }
     void UpdatePath()
     {
@@ -42,7 +48,6 @@ public class Enemy_AI : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (path == null)
@@ -51,6 +56,7 @@ public class Enemy_AI : MonoBehaviour
         if (currentWaypoint >= path.vectorPath.Count)
         {
             reachedEndOfPath = true;
+            AnimateMovement(new Vector2(0f, 0f));
             return;
         }
         else
@@ -62,7 +68,9 @@ public class Enemy_AI : MonoBehaviour
         Vector2 force = direction * moveSpeed * Time.deltaTime;
 
         rb.AddForce(force);
+        AnimateMovement(direction);
 
+        Debug.Log("X: " + direction.x + " | Y: " + direction.y);
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
         if (distance < nextWaypointDistance)
@@ -70,7 +78,47 @@ public class Enemy_AI : MonoBehaviour
             currentWaypoint++;
         }
 
+    }
 
+    void AnimateMovement(Vector2 moveDirection)
+    {
+        if (((Mathf.Abs(moveDirection.x) > 0.4f && (Mathf.Abs(rb.velocity.x) > 0.01f)) ||
+            (Mathf.Abs(moveDirection.x) > 0f && (Mathf.Abs(moveDirection.y) < 0.2f) && (Mathf.Abs(rb.velocity.y) > 0.01f)))
+            && (Mathf.Abs(rb.velocity.x) > 0.01f))
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetInteger("facingDirection", 1);
+
+            //Flip if needed
+            if (rb.velocity.x >= 0.01f && moveDirection.x > 0f && !facingRight)
+            {
+                enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+                facingRight = true;
+            }
+            else if (rb.velocity.x <= -0.01 && moveDirection.x < 0f && facingRight)
+            {
+                Debug.Log(animator.GetBool("isWalking"));
+                enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+                facingRight = false;
+            }
+        }
+        else if (Mathf.Abs(moveDirection.y) > 0f && (Mathf.Abs(rb.velocity.y) > 0.01f))
+        {
+            //
+            animator.SetBool("isWalking", true);
+            if (moveDirection.y > 0f)
+                animator.SetInteger("facingDirection", 2);
+            else
+                animator.SetInteger("facingDirection", 0);
+
+        }
+        else if ((moveDirection.x == 0f && moveDirection.y == 0f) && ((Mathf.Abs(rb.velocity.x) < 0.01f) && (Mathf.Abs(rb.velocity.y) < 0.01f)))
+        {
+                //No Movement
+                animator.SetBool("isWalking", false);
+                Debug.Log("No Movement");
+
+        }
 
     }
 }
